c506c777c78c58409d7ea25a822d8ac9dc949819
MaxiProger <kolotseymax@gmail.com>
2018-10-19 15:44:32 +0300

    Changed observable to single

--- app/src/main/java/com/example/liban/rxjavatest/Api.java
+++ app/src/main/java/com/example/liban/rxjavatest/Api.java
@@ -2,14 +2,11 @@ package com.example.liban.rxjavatest;
 
 import java.util.List;
 
-
-import io.reactivex.Observable;
-import retrofit2.Call;
+import io.reactivex.Single;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
 
 
 public interface Api {
     @GET("posts")
-    Observable<List<Users>> getUsers();
+    Single<List<Users>> getUsers();
 }
